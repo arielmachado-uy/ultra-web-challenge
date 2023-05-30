ae980becf30874ff858b24dbb778fc4de3b5a6b5
Ariel Machado <arielma77@gmail.com>
2023-05-30 19:33:40 -0300

    First Login scenario
    POC logging in using the UI

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,9 @@
 const { defineConfig } = require("cypress");
 
 export default defineConfig({
-  defaultCommandTimeout: 30000,
-  responseTimeout: 30000,
-  requestTimeout: 30000,
+  chromeWebSecurity: false,
   e2e: {
-    baseUrl: "https://www.saucedemo.com",
+    baseUrl: "https://www.saucedemo.com/", 
   },
 
   async setupNodeEvents(on: any, config: any) {
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,6 +1,14 @@
+import { Login } from '../fixtures/login-page'
 
-describe("Log in suite", function () {
+describe("Log in suite", () => {
+  beforeEach(() => {
+    cy.visit('/');
+  })
+
+  const loginPage = new Login();
+
   it("Log in with an existing email address", function () {
-    cy.visit("/");
+    loginPage.enterCredentials('standard_user', 'secret_sauce');
+    loginPage.getLoginButton().click();
   });
 })
